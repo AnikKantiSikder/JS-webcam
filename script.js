406a6d57ec7caa461aa624ab39f99e26c60a3d79
Anik <sikder35-1408@diu.edu.bd>
2022-04-04 22:50:34 +0600

    webcam with javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// const camera = document.getElementById('camera');
+//const result = document.getElementById('results');
+// const clickBtn = document.getElementById('btn');
+
+// Webcam.set({
+//     width:350,
+//     height:350,
+//     image_format: 'jpeg',
+//     jpeg_quality: 90
+// });
+
+// Webcam.attach('#camera');
+
+// clickBtn.addEventListener('onclick', function() {
+
+//     Webcam.snap( function(data_uri) {
+//         document.getElementById('results').innerHTML = 
+//          '<img src="'+data_uri+'"/>';
+//     } );
+
+// })
